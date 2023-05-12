a7366c4b7e8ca83656141ed4f5f86af055cfdf26
Abdirahman Shukri <abdirahmanshukri@Abdirahmans-MacBook-Air.local>
2023-05-12 01:20:37 -0500

    final work

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,27 +2,57 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
-
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
-
-router.post('/', (req, res) => {
-  // create a new tag
-});
-
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
-
-module.exports = router;
+  try {
+    const tagData = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'image_url'],
+          },
+          ],
+          });
+          const tags = tagData.map((tag) => tag.get({ plain: true }));
+          res.status(200).json(tags);
+          } catch (err) {
+            res.status(500).json(err);
+            }
+            });
+            // POST route for adding a new tag
+            router.post('/', async (req, res) => {
+              // create a new tag
+              try {
+                const tagData = await Tag.create({
+                  tag_name: req.body.tag_name,
+                  });
+                  // add the new tag to the product
+                  const newTag = await Tag.findByPk(tagData.id, {
+                    include: [
+                      {
+                        model: Product,
+                        attributes: ['id', 'product_name', 'price', 'stock', 'image_url'],
+                        },
+                        ],
+                        });
+                        res.status(200).json(newTag);
+                        } catch (err) {
+                          res.status(500).json(err);
+                          }
+                          });
+                          // DELETE route for removing a tag
+                          router.delete('/:id', async (req, res) => {
+                            // delete a tag by its `id` value
+                            try {
+                              const tagData = await Tag.destroy({
+                                where: {
+                                  id: req.params.id,
+                                  },
+                                  });
+                                  res.status(200).json(tagData);
+                                  } catch (err) {
+                                    res.status(500).json(err);
+                                    }
+                                    });
+                                    module.exports = router;
